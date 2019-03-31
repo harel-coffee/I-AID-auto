00fe6dade82201b1f2cd23eb4ce8ed1abdadedcb
Ibrahim Elgendy <eng.elgendy90@gmail.com>
2019-03-31 09:49:20 +0800

    Tweets Dataset is added to Data/Tweets Data Directory

--- Data/TREC_Tweets/JSONtoCSV.py
+++ Data/TREC_Tweets/JSONtoCSV.py
@@ -9,6 +9,7 @@ def tweets_JSON_CSV(path):
             if file.endswith('.json'):
                 #print(dirs+file)
                 with open(dirs+file, 'r',encoding='utf-8') as input_file:
+                    print(dirs+file)
                     csv_out = open(dirs+file+'.csv', mode='w') #opens csv file
                     writer = csv.writer(csv_out) #create the csv writer object
                     fields=['tweet_id','Text','TimeStamp','Event_Type','User Info', 'MetaData','Place']
@@ -23,5 +24,9 @@ def tweets_JSON_CSV(path):
                             continue
 
 
-tweets_JSON_CSV('E:/HIT/TREC-IS/Implementation/TrainingData/')
-tweets_JSON_CSV('E:/HIT/TREC-IS/Implementation/Test Data/')
+#Train Data
+Training_Data_Path='E:/HIT/TREC-IS/Implementation/TREC_IS 2019 Code/TREC-IS19/Data/Tweets Data/Train Data/'
+tweets_JSON_CSV(Training_Data_Path)
+#Test Data
+Test_Data_Path='E:/HIT/TREC-IS/Implementation/TREC_IS 2019 Code/TREC-IS19/Data/Tweets Data/Test Data/'
+tweets_JSON_CSV(Test_Data_Path)

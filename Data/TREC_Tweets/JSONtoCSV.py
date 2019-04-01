2eca029e938b6772788609469c92cc31a293e0db
Ibrahim Elgendy <eng.elgendy90@gmail.com>
2019-04-01 11:38:57 +0800

    CSV files for tweets are added

--- Data/TREC_Tweets/JSONtoCSV.py
+++ Data/TREC_Tweets/JSONtoCSV.py
@@ -10,16 +10,16 @@ def tweets_JSON_CSV(path):
                 #print(dirs+file)
                 with open(dirs+file, 'r',encoding='utf-8') as input_file:
                     print(dirs+file)
-                    csv_out = open(dirs+file+'.csv', mode='w') #opens csv file
+                    csv_out = open(dirs+file+'.csv', mode='w',newline='') #opens csv file
                     writer = csv.writer(csv_out) #create the csv writer object
-                    fields=['tweet_id','Text','TimeStamp','Event_Type','User Info', 'MetaData','Place']
+                    fields=['Event_Name','tweet_id','Text','TimeStamp','User Info', 'Description']
                     writer.writerow(fields) #writes field
                     for line in input_file:
                         try:
                             tweet = json.loads(line)
                             AllProperties=tweet['allProperties']                            
                             Event_Name=tweet['topic']
-                            writer.writerow([AllProperties['id'],AllProperties['text'],AllProperties['user.created_at'],Event_Name])                        
+                            writer.writerow([Event_Name,AllProperties['id'],AllProperties['text'],AllProperties['created_at'],AllProperties['user.name'],AllProperties['user.description']])
                         except:
                             continue
 
